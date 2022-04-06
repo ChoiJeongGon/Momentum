95fc172ee9dfbf6e0c6f023e1f44477c5bd80778
최정곤 <92897721+ChoiJeongGon@users.noreply.github.com>
2022-04-06 16:28:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// const constant (상수) = 값이 바뀔 수 없음 (업데이트 X)
+// let = 새로운 것을 생성 할때 (업데이트 O)
+
+// const a = 5;
+// const b = 2;
+// let myName = "cho";
+
+// console.log(a + b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log("hello " + myName);
+
+// myName = "chots";
+
+// console.log("you new name is " + myName);
+
+// const amIFat = false;
+// let something;
+// console.log(something , amIFat);
+
+// const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// array 에서 아이템 받기
+// console.log(daysOfWeek);
+
+// // array 에서 요일 추가 (push)
+// daysOfWeek.push("sun");
+
+// console.log(daysOfWeek);
+
+// const toBuy = ["potato", "tomato","pizza"];
+// console.log(toBuy[2]);
+
+// Objects  // property(특성)
+const player = {
+  name: "cho",
+  points: 10,
+  fat: false,
+};
+console.log(player);
+player.lastName = "potato";
+player.points = player.points + 15;
+console.log(player.points);
