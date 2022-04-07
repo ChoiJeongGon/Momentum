c54dbc3bbe577af209cb7b45e985b3be5248b7fa
최정곤 <92897721+ChoiJeongGon@users.noreply.github.com>
2022-04-07 16:44:36 +0000

    2/8 study

--- javasc4.js
+++ javasc4.js
@@ -10,7 +10,7 @@ if(isNaN(age) || age < 0){
 }else if(age > 50 && age <= 80){
   console.log("You should exercise");
 }else if(age === 100){
-  console.log("wow you are wise");
+  console.log("wow you are wise!!!");
 }else if(age > 80){
   console.log("You can do wantever you want.");
 }
