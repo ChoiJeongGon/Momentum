0085096b6fbdd1d4fc39dbeef5120bf5b4675b52
Momentum <shinbaji@gmail.com>
2022-04-10 23:25:33 +0900

    upload

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
-const title = document.querySelector(".hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
+
+console.dir(Element);
+
 
 function handleTitleClick(){
-    title.style.color="blue";
-    console.log("title was clicked!");
+    h1.style.color="blue";
 }
 
-title.addEventListener("click", handleTitleClick);
+function handleMouseEnter(){
+    h1.innerText = "Mouse is here!";
+}
+function handleMouseleave(){
+    h1.innerText = "Mouse is gone!";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+
+function handleWindowOffline(){
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline(){
+    alert("ALL GOOD");
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter",handleMouseEnter);
+h1.addEventListener("mouseleave",handleMouseleave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
