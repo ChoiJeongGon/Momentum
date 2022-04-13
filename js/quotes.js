336216287ac11e8dc03c4bbe634d21373334efc8
Momentum <shinbaji@gmail.com>
2022-04-13 18:01:58 +0900

    quotes Upload

--- js/quotes.js
+++ js/quotes.js
@@ -42,7 +42,7 @@ const quotes = [
     ];
 
     const quote = document.querySelector('#quote span:first-child');
-    const author = document.querySelector('#quots span:last-child');
+    const author = document.querySelector('#quote span:last-child');
     const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
     quote.innerText = todaysQuote.quote;
